148d24b2a2a8145494920cf464ace7c466f4b855
Lucas Reis <lucasmreis@gmail.com>
2017-06-11 22:14:54 -0300

    initial

---
+++ src/App.fs
@@ -0,0 +1,73 @@
+module TheCube
+
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.Import
+
+importSideEffects "./styles.css"
+
+type Sides = Front | Left | Main | Right
+type TopOrBottom = Top | Bottom
+
+type ShowingSide =
+    | Side of Sides
+    | TopOrBottom of TopOrBottom
+
+type TurnTo = ToRight | ToLeft | ToTop | ToBottom
+
+let turn showing turnTo =
+    match showing, turnTo with
+    | TopOrBottom _, ToRight -> Side Right
+    | TopOrBottom _, ToLeft -> Side Left
+
+    | TopOrBottom Top, ToTop -> Side Main
+    | TopOrBottom Top, ToBottom -> Side Front
+    | TopOrBottom Bottom, ToTop -> Side Front
+    | TopOrBottom Bottom, ToBottom -> Side Main
+
+    | Side _, ToTop -> TopOrBottom Top
+    | Side _, ToBottom -> TopOrBottom Bottom
+
+    | Side Front, ToRight -> Side Right
+    | Side Front, ToLeft -> Side Left
+
+    | Side Right, ToRight -> Side Main
+    | Side Right, ToLeft -> Side Front
+
+    | Side Main, ToRight -> Side Left
+    | Side Main, ToLeft -> Side Right
+
+    | Side Left, ToRight -> Side Front
+    | Side Left, ToLeft -> Side Main
+
+let show position =
+    match position with
+    | Side Front -> "rotateX(0deg) rotateY(0deg)"
+    | Side Left -> "rotateX(0deg) rotateY(90deg)"
+    | Side Right -> "rotateX(0deg) rotateY(-90deg)"
+    | Side Main -> "rotateX(0deg) rotateY(180deg)"
+    | TopOrBottom Top -> "rotateX(-90deg) rotateY(0deg)"
+    | TopOrBottom Bottom -> "rotateX(90deg) rotateY(0deg)"
+
+let update (cube: Browser.HTMLElement) showing turnTo =
+    let next = turn showing turnTo
+    cube.style.transform <- show next
+    Browser.console.log (sprintf "Showing: %A" next)
+    next
+
+let init () =
+    let mutable showing = Side Front
+
+    let cube = Browser.document.getElementById "cube"
+
+    let up = Browser.document.getElementById "btn-up"
+    let down = Browser.document.getElementById "btn-down"
+    let left = Browser.document.getElementById "btn-left"
+    let right = Browser.document.getElementById "btn-right"
+
+    up.addEventListener_click(fun _ -> showing <- update cube showing ToTop; null)
+    down.addEventListener_click(fun _ -> showing <- update cube showing ToBottom; null)
+    left.addEventListener_click(fun _ -> showing <- update cube showing ToLeft; null)
+    right.addEventListener_click(fun _ -> showing <- update cube showing ToRight; null)
+
+init () |> ignore
